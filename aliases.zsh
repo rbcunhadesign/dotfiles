f4d9081213d82d09d3463561ef9034b83448a937
Raphael Cunha <rbcunhadesign@gmail.com>
2023-01-24 18:24:15 -0500

    Adjust aliases

--- aliases.zsh
+++ aliases.zsh
@@ -8,6 +8,7 @@ alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 alias compile="commit 'compile'"
 alias version="commit 'version'"
+alias aliasconfig="vim ~/.dotfiles/aliases.zsh"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -17,11 +18,14 @@ alias lara="sites && cd laravel/"
 alias docs="lara && cd docs/"
 
 # Laravel
-alias a="php artisan"
-alias fresh="php artisan migrate:fresh --seed"
+alias pa="php artisan"
+alias pamfs="php artisan migrate:fresh --seed"
 alias tinker="php artisan tinker"
-alias seed="php artisan db:seed"
+alias paseed="php artisan db:seed"
 alias serve="php artisan serve"
+alias pu="php artisan test"
+alias puf="php artisan test --filter"
+alias pug="php artisan test --group"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -30,23 +34,23 @@ alias composer="php -d memory_limit=-1 /opt/homebrew/bin/composer"
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
-
-# Docker
-alias docker-composer="docker-compose"
+alias dev="npm run dev"
 
 # SQL Server
 alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
 
 # Git
+alias master="git checkout master"
+alias main="git checkout main"
 alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
+alias gcb="git branch"
+alias gco="git checkout"
 alias gl="git log --oneline --decorate --color"
 alias amend="git add . && git commit --amend --no-edit"
 alias commit="git add . && git commit -m"
 alias diff="git diff"
 alias force="git push --force"
-alias nuke="git clean -df && git reset --hard"
+alias nah="git clean -df && git reset --hard"
 alias pop="git stash pop"
 alias pull="git pull"
 alias push="git push"
